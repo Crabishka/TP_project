033aa9e91320e37262637992ca0b3686cf9de825
Gorginio <65155752+Crabishka@users.noreply.github.com>
2023-04-08 14:18:34 +0300

    Add files via upload

---
+++ BankMock/src/main/java/com/example/bankMock/BankMockApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankMock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankMockApplication.class, args);
+	}
+
+}
---
+++ BankMock/src/test/java/com/example/bankMock/BankMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankMock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
