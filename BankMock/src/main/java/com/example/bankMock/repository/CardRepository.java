e04a2a8c9625763f97fc08fe226207a43ed7b2da
JohnyTwoJacket <hagoga.pr@gmail.com>
2023-04-26 22:34:14 +0300

    init backend_develop

--- BankMock/src/main/java/com/example/bankMock/repository/CardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bankMock.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CardRepository extends JpaRepository<Card, Long> {
-    // методы для работы с данными банковских карт
-}
--- BankMock/src/main/java/com/example/bankMock/repository/CustomerRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.bankMock.repository;
-
-@Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    // методы для работы с данными клиентов
-}
