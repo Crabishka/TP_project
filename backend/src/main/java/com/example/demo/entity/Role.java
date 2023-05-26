70da16c5213274e4c272b51e33f543bb5b30bae5
JohnyTwoJacket <hagoga.pr@gmail.com>
2023-05-26 13:24:05 +0300

    add submodules

--- backend/src/main/java/com/example/demo/entity/Role.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.entity;
-
-public enum Role{
-    USER,
-    EMPLOYEE
-}
--- backend/src/main/java/com/example/demo/repository/ProductPropertiesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ProductProperty;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductPropertiesRepository extends JpaRepository<ProductProperty, Long> {
-}
